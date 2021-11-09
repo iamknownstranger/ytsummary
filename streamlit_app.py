b3b1cac1f45ef92e8048946019c6000346900360
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2021-11-09 21:51:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,8 @@ st.title("Youtube Summarizer")
 st.header("This application help you to get the summary of a youtube video")
 video_url = st.text_input("Enter youtube video URL: [you can try https://www.youtube.com/watch?v=j70AA9arThc]")
 button = st.button("Get Summary")
-
-if button:
-    try:
+try:
+    if button:
         transcript_corpus = get_transcript(video_url)
         st.header("Transcript")
         with st.expander("View Transcript"):
@@ -17,9 +16,9 @@ if button:
         with st.expander("View Summary"):
             
             st.write(summary)
-    except Exception as e:
-        st.warning("Whooops! Could not retrieve a transcript for the video")
-        st.exception(e)
+except Exception as e:
+    st.warning("Whooops! Could not retrieve a transcript for the video")
+    st.exception(e)
     
     
 
